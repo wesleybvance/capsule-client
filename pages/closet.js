555d530ef9a6408366b02debad39a714645f5217
Wesley Vance <wslyvnce@gmail.com>
2023-08-23 21:00:15 -0500

    build category select component

--- pages/closet.js
+++ pages/closet.js
@@ -1,33 +1,57 @@
 import React, { useEffect, useState } from 'react';
-// import { Carousel } from 'react-bootstrap';
-// import { useAuth } from '../utils/context/authContext';
-// import ItemCarousel from '../components/Items/ItemCarousel';
-import { getAllItems } from '../utils/data/itemData';
+import { FloatingLabel, Form } from 'react-bootstrap';
+import { getItemsByUser, getItemsByUserCategory } from '../utils/data/itemData';
 import ItemCard from '../components/Items/ItemCard';
+import { useAuth } from '../utils/context/authContext';
+import { getAllCategories } from '../utils/data/categoryData';
+import CategorySelect from '../components/Items/CategorySelect';
 
 export default function Closet() {
-  // const { user } = useAuth();
+  const { user } = useAuth();
+  const [formSelect, setFormSelect] = useState({
+    category: '',
+  });
   const [items, setItems] = useState([]);
+  const [categories, setCategories] = useState([]);
 
-  const getClosetItems = () => {
-    getAllItems().then(setItems);
-    console.warn(items);
+  const getItemsByCategory = (categoryId) => {
+    getItemsByUserCategory(user.id, categoryId).then(setItems);
   };
 
   useEffect(() => {
-    getClosetItems();
+    getAllCategories().then((data) => setCategories(data));
+    if (formSelect.category === 'none') {
+      getItemsByUser(user.id).then(setItems);
+    } else if (formSelect.name) {
+      getItemsByCategory(formSelect.id);
+    } else {
+      getItemsByUser(user.id).then(setItems);
+    }
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [formSelect.id]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormSelect((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
   return (
-    <div>
+    <div className="main-closet-cont">
+      <div className="closet-select">
+        <Form>
+          <FloatingLabel controlId="floatingInput1" label="Category" className="mb-3">
+            <Form.Select aria-label="Select Category" required name="category" onChange={handleChange}>
+              <option value="none">All Clothing Items</option>
+              {categories.map((category) => (<CategorySelect key={category.id} id={category.id} name={category.name} />))}
+            </Form.Select>
+          </FloatingLabel>
+        </Form>
+      </div>
       {items ? items.map((item) => (
         <ItemCard key={item.id} name={item.name} categoryId={item.category_id} photoUrl={item.photo_url} />)) : 'You have no items'}
     </div>
-    // <Carousel className="mt-5">
-    //   {items ? items.map((item) => (
-    //     <ItemCarousel key={item.id} name={item.name} categoryId={item.category_id} photoUrl={item.photo_url} />
-    //   )) : 'You have no items'}
-    // </Carousel>
   );
 }
