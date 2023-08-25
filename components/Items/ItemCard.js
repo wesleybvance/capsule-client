b6385da68e772eba3d2c60aba2c8782e06266df3
Wesley Vance <wslyvnce@gmail.com>
2023-08-25 16:47:48 -0500

    delete working for items and outfits

--- components/Items/ItemCard.js
+++ components/Items/ItemCard.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { getSingleCategory } from '../../utils/data/categoryData';
+import { deleteItem } from '../../utils/data/itemData';
 
 export default function ItemCard({
-  categoryId, photoUrl, name,
+  iid, categoryId, photoUrl, name, onUpdate,
 }) {
   const [category, setCategory] = useState();
 
@@ -12,11 +13,18 @@ export default function ItemCard({
     getSingleCategory(id).then(setCategory);
   };
 
+  const deleteThisItem = () => {
+    if (window.confirm('Are you sure you want to delete this item from your closet? This will remove the item from any outfits it is in.')) {
+      deleteItem(iid).then(() => onUpdate());
+    }
+  };
+
   useEffect(() => {
     getCategoryInfo(categoryId);
   }, [categoryId]);
   return (
     <Card style={{ width: '18rem' }}>
+      <Button variant="light" className="btn btn-outline-danger" onClick={deleteThisItem}>x</Button>
       <Card.Img variant="top" src={photoUrl} />
       <Card.Body>
         <Card.Title>{name}</Card.Title>
@@ -30,4 +38,6 @@ ItemCard.propTypes = {
   name: PropTypes.string.isRequired,
   photoUrl: PropTypes.string.isRequired,
   categoryId: PropTypes.number.isRequired,
+  iid: PropTypes.number.isRequired,
+  onUpdate: PropTypes.func.isRequired,
 };
