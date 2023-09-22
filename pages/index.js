0b871057ba6730e41be47d07d5814a1760a7987a
Wesley Vance <wslyvnce@gmail.com>
2023-09-22 17:53:32 -0500

    added links to homepage, slight styling fixes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import WelcomeUser from '../components/WelcomeUser';
 
 function Home() {
@@ -10,6 +11,12 @@ function Home() {
         <title>Home</title>
       </Head>
       <WelcomeUser />
+      <div className="cflex cleft margin15">
+        <Link passHref href="/closet"><h3 className="racing-sans m-3 font30 home-btn">❀  view your closet </h3></Link>
+        <Link passHref href="/items/new"><h3 className="racing-sans m-3 font30 home-btn">✻  add item to closet</h3></Link>
+        <Link passHref href="/outfits/new"><h3 className="racing-sans m-3 font30 home-btn">✰  create an outfit</h3></Link>
+        <Link passHref href="/outfits/all"><h3 className="racing-sans m-3 font30 home-btn">✭  see your outfits</h3></Link>
+      </div>
     </div>
   );
 }
